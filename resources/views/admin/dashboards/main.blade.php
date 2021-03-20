3a4c1ab1c9e8235385c2ba0328ee4ae1f0350b9d
Svetlana Dmitrieva <53534598+Violetale@users.noreply.github.com>
2021-03-20 14:14:25 +0300

    Create main sections in the admin panel with dashboards.

---
+++ resources/views/admin/dashboards/main.blade.php
@@ -0,0 +1,16 @@
+@extends("prometheus::admin.layouts.admin_panel")
+@section("content")
+    <div class="position-relative mb-4">
+        <canvas id="visitors-chart" height="200"></canvas>
+    </div>
+
+    <div class="d-flex flex-row justify-content-end">
+                  <span class="mr-2">
+                    <i class="fas fa-square text-primary"></i> This Week
+                  </span>
+
+        <span>
+                    <i class="fas fa-square text-gray"></i> Last Week
+                  </span>
+    </div>
+@endsection
